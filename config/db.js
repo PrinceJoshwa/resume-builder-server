a694333698e8f1088abdd9a27370ac21e737fc9c
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-04 01:36:01 +0530

    auth server changes commit

--- config/db.js
+++ config/db.js
@@ -1,17 +1,33 @@
 
 const mongoose = require('mongoose');
 
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('MongoDB connection error:', error);
+//     process.exit(1); // Exit process with failure
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1); // Exit process with failure
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
   }
-};
-
+}
+// connectDB().catch(console.dir);
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 connectDB();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://resume-builder-client-jade.vercel.app',
     credentials: true, 
   }));
   
@@ -76,5 +76,6 @@ app.get('/dashboard', isAuthenticated, (req, res) => {
 });
 
 // Server Start
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
