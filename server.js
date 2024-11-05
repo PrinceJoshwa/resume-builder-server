03c865851d77736f9ef0362473f0c643cac5fb16
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-06 00:26:44 +0530

    auth changes

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.use(cors({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SEC,
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'https://resume-builder-server-roan.vercel.app/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
